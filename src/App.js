491e58bf827850ce736d8ad6fee067b084de9550
Laura Kambanis <kambanislaura@gmail.com>
2022-11-19 17:17:48 +0000

    bug fixed- component put inside App(), profile image not rendering-bug

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import MainPage from "./pages";
+// import MainPage from "./pages";
 import Nav from "./components/Navigation";
+import AboutMe from "./pages/AboutMe";
 
 function App() {
   return (
     <div className="App">
       <Nav className="App-nav" />
       <Header className="App-header" />
-      <MainPage />
+      <AboutMe />
       <Footer className="App-footer" />
     </div>
   );
