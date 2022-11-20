4224ab5df57f683a35f2315c48aaae73454bda60
Laura Kambanis <kambanislaura@gmail.com>
2022-11-20 15:12:42 +0000

    added input fields in contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,48 @@
 import React from "react";
+import { MDBInput, MDBRow, MDBCol, MDBTextArea } from "mdb-react-ui-kit";
 
 export default function Contact() {
   return (
-    <div className="aboutMe">
-      {/* <img src={ProfilePic} className="profile-picture" alt="profile" /> */}
-      <p className="aboutMe">
-        CONTACT ME
-        <br />
-      </p>
+    <div>
+      <MDBRow className="g-3 align-items-center">
+        <MDBInput
+          wrapperClass="col-auto"
+          label="First and Last Name"
+          type="text"
+          id="formTextExample2"
+          aria-describedby="textExample2"
+        />
+        <MDBCol size="auto">
+          <span id="textExample2" className="form-text">
+            Must be 8-20 characters long.
+          </span>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow className="g-3 align-items-center">
+        <MDBInput
+          wrapperClass="col-auto"
+          label="Email"
+          type="text"
+          id="formTextExample2"
+          aria-describedby="textExample2"
+        />
+        <MDBCol size="auto">
+          <span id="textExample2" className="form-text">
+            Must be 8-20 characters long.
+          </span>
+        </MDBCol>
+      </MDBRow>
+      <div>
+        <MDBTextArea label="Message" id="textAreaExample" rows={4} />
+      </div>
     </div>
   );
+  // <div className="aboutMe">
+  //   {/* <img src={ProfilePic} className="profile-picture" alt="profile" /> */}
+  //   <p className="aboutMe">
+  //     CONTACT ME
+  //     <br />
+  //   </p>
+  // </div>
+  //   );
 }
