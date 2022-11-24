6e311c6655b36ee2c4dd6182284cdfd86021171d
Laura Kambanis <kambanislaura@gmail.com>
2022-11-24 22:27:42 +0000

    added error messages fo rinput field

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  validateEmail,
+  validateUserName,
+  validateMessage,
+} from "../utils/helpers";
+// Importing a helper function
+
 import {
   MDBInput,
   MDBRow,
@@ -16,6 +23,30 @@ import "./pages.css";
 
 //Contact JSX name/email and message
 export default function Contact() {
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleBlur = (e) => {
+    if (e.target.name === "name") {
+      if (!validateUserName(e.target.value)) {
+        setErrorMessage("Username required");
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (e.target.name === "message") {
+      if (!validateMessage(e.target.value)) {
+        setErrorMessage("Message required");
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (e.target.name === "email") {
+      if (!validateEmail(e.target.value)) {
+        setErrorMessage("A valid email is required");
+      } else {
+        setErrorMessage("");
+      }
+    }
+  };
   return (
     <MDBCard alignment="center">
       <MDBCardHeader></MDBCardHeader>
@@ -32,6 +63,8 @@ export default function Contact() {
               type="text"
               id="formTextExample2"
               aria-describedby="textExample2"
+              name="name"
+              onBlur={handleBlur}
             />
             <MDBCol size="auto">
               <span id="textExample2" className="form-text">
@@ -46,6 +79,8 @@ export default function Contact() {
               type="text"
               id="formTextExample2"
               aria-describedby="textExample2"
+              name="email"
+              onBlur={handleBlur}
             />
             <MDBCol size="auto">
               <span id="textExample2" className="form-text">
@@ -54,8 +89,15 @@ export default function Contact() {
             </MDBCol>
           </MDBRow>
           <div>
-            <MDBTextArea label="Message" id="textAreaExample" rows={5} />
+            <MDBTextArea
+              label="Message"
+              id="textAreaExample"
+              rows={5}
+              name="message"
+              onBlur={handleBlur}
+            />
           </div>
+          <p>{errorMessage}</p>
         </div>
         <MDBBtn href="#">Submit</MDBBtn>
       </MDBCardBody>
